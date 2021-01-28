5f8bfcafa0e685e69ec7e3524ea90202b5d8ecd6
r4jiv007 <r4jiv007@gmail.com>
2021-01-28 13:30:29 +0400

    added camera, album, share and cancel button to the screen with toolbar and imageView

--- MemeMe1/ViewController.swift
+++ MemeMe1/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+                      UINavigationControllerDelegate {
 
+    @IBOutlet weak var shareMeme: UIBarButtonItem!
+    @IBOutlet weak var pickImage: UIBarButtonItem!
+    @IBOutlet weak var pickCamera: UIBarButtonItem!
+    @IBOutlet weak var memeImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func pickAnImage(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
 
+    }
+    
+    @IBAction func pickCamera(_ sender: Any) {
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imageData = info[.originalImage] as? UIImage{
+            memeImage.image=imageData
+        }
+            dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
